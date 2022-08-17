792ed6a4b6c66e078ce8824fcaf38a746de03cc3
Jacob Hood <jacobwhood@gmail.com>
2022-08-17 11:56:16 -0700

    Implement search behavior (#4)

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,49 @@
 import React from "react";
+import agent from "../../agent";
 import logo from "../../imgs/logo.png";
+import { APPLY_TITLE_FILTER } from "../../constants/actionTypes";
+import { connect } from "react-redux";
 
-const Banner = () => {
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateItems: (payload) =>
+      dispatch({ type: APPLY_TITLE_FILTER, payload: payload }),
+  };
+};
+
+const Banner = (props) => {
+  const [searchText, setSearchText] = React.useState("");
+
+  const handleUpdateSearchInput = async (e) => {
+    setSearchText(e.target.value);
+    if (e.target.value.length >= 3) {
+      console.log("searching for items with title: ", e.target.value);
+      const items = await agent.Items.byTitle(e.target.value);
+      console.log("items: ", items);
+      props.updateItems(items);
+    }
+  };
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <div className="search-box-wrapper">
+            <input
+              id="search-box"
+              value={searchText}
+              onChange={handleUpdateSearchInput}
+              placeholder={"What is it that you truly desire?"}
+            />
+            <img
+              src={
+                "https://upload.wikimedia.org/wikipedia/commons/5/55/Magnifying_glass_icon.svg"
+              }
+              alt="magnifying glass icon"
+            />
+          </div>
           <span> the cool stuff.</span>
         </div>
       </div>
@@ -15,4 +51,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export default connect(null, mapDispatchToProps)(Banner);
